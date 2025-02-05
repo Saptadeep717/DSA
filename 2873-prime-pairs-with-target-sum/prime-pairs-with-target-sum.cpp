e6b1a70f785185caf6c6bbc3083002cd9a9f3534
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-02-05 23:46:24 +0530

    Time: 285 ms (60.06%) | Memory: 313.4 MB (5.54%) - LeetSync

--- 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.cpp
+++ 2873-prime-pairs-with-target-sum/prime-pairs-with-target-sum.cpp
@@ -16,18 +16,15 @@ public:
         vector<vector<int>>ans;
         vector<long long int>sieve = primeCalculate(n);
 
-        map<int,int>mpp;
-        for(auto i=0;i<=n;i++){
-            if(sieve[i]==1) mpp.insert({i,i});
-        }
+        // map<int,int>mpp;
+        // for(auto i=0;i<=n;i++){
+        //     if(sieve[i]==1) mpp.insert({i,i});
+        // }
 
         for(int i=2;i<=n/2;i++){
 
-            if(mpp.count(i) && mpp.count(n-i)){
-                ans.push_back({i,n-i});
-                mpp.erase(i);
-                mpp.erase(n-i);
-            }
+            int key = n-i;
+            if(sieve[i] && sieve[key]) ans.push_back({i,key});
         }
 
        
