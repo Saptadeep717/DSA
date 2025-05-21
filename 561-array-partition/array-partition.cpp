5a74881b5a24a405e4add03ed9a0e725d3c13055
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-05-21 18:46:04 +0530

    Time: 13 ms (40.15%) | Memory: 32.2 MB (18.19%) - LeetSync

---
+++ 561-array-partition/array-partition.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+
+        int n = nums.size();
+        int sum=0;
+        for(int i=n-2;i>=0;i-=2){
+            sum+= nums[i];
+        }
+        return sum;
+    }
+};
