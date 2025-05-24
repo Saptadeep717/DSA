2f7ffebce7baabf5b3295a2f4572a337b6e71af7
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-05-24 19:17:07 +0530

    Time: 21 ms (39.00%) | Memory: 26.3 MB (38.33%) - LeetSync

--- 978-valid-mountain-array/valid-mountain-array.cpp
+++ 978-valid-mountain-array/valid-mountain-array.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
     bool validMountainArray(vector<int>& arr) {
-        int n = arr.size();
-        if(n<3) return false;
-        if(arr[0]>arr[1]) return false;
-        int ind=-1,ind2=-1;
-        for(int i =0;i<n-1;i++){
-            if(arr[i+1] <= arr[i]){
-                ind=i;
-                break;
-            }
+        // int n = arr.size();
+        // if(n<3) return false;
+        // if(arr[0]>arr[1]) return false;
+        // int ind=-1,ind2=-1;
+        // for(int i =0;i<n-1;i++){
+        //     if(arr[i+1] <= arr[i]){
+        //         ind=i;
+        //         break;
+        //     }
 
-        }
-        if(ind==-1) return false;
-        for(int i=ind;i<n-1;i++){
-            if(arr[i+1] >= arr[i]){
-                ind2=i;
-                break;
-            }
-        }
+        // }
+        // if(ind==-1) return false;
+        // for(int i=ind;i<n-1;i++){
+        //     if(arr[i+1] >= arr[i]){
+        //         ind2=i;
+        //         break;
+        //     }
+        // }
 
-        if(ind2!=-1) return false;
-        return true;
+        // if(ind2!=-1) return false;
+        // return true;
+
+        int n = arr.size(), i = 0, j = n - 1;
+        while (i + 1 < n && arr[i] < arr[i + 1]) i++;
+        while (j > 0 && arr[j - 1] > arr[j]) j--;
+        return i > 0 && i == j && j < n - 1;
     }
 };
