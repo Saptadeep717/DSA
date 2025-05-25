6d379e7964d99236371df40eeb07d9d754928cca
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-05-25 19:30:38 +0530

    Time: 6 ms (47.85%) | Memory: 41.4 MB (24.43%) - LeetSync

--- 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
+++ 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
@@ -4,16 +4,26 @@ public:
         return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100));
     }
     int numFriendRequests(vector<int>& ages) {
-         sort(ages.begin(), ages.end());
-        int cnt=0;
-        for(auto age : ages){
-            int i=upper_bound(ages.begin(),ages.end(),age)-ages.begin();
+        //  sort(ages.begin(), ages.end());
+        // int cnt=0;
+        // for(auto age : ages){
+        //     int i=upper_bound(ages.begin(),ages.end(),age)-ages.begin();
 
-             int j=upper_bound(ages.begin(),ages.end(),age*0.5 + 7)-ages.begin();
+        //      int j=upper_bound(ages.begin(),ages.end(),age*0.5 + 7)-ages.begin();
 
-             cnt+=max(0,i-j-1);
-        }
+        //      cnt+=max(0,i-j-1);
+        // }
+    //return cnt;
+        unordered_map<int, int> count;
+        for (int &age : ages)
+            count[age]++;
+        int res = 0;
+        for (auto &a : count)
+            for (auto &b : count)
+                if (request(a.first, b.first))
+                    res += a.second * (b.second - (a.first == b.first ? 1 : 0));
+        return res;
 
-        return cnt;
+        
     }
 };
