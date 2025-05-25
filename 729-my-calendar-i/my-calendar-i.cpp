f6e84e85355a4be609c14f5ce27f7509d0b45590
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-05-26 04:19:36 +0530

    Time: 7 ms (94.27%) | Memory: 44.2 MB (31.84%) - LeetSync

--- 729-my-calendar-i/my-calendar-i.cpp
+++ 729-my-calendar-i/my-calendar-i.cpp
@@ -1,15 +1,23 @@
 class MyCalendar {
-    vector<pair<int, int>> books;
+    // vector<pair<int, int>> books;
+    map<int, int> mpp;
 
 public:
     MyCalendar() {}
 
-    bool book(int startTime, int endTime) {
-        for (auto p : books) {
-            if (max(p.first, startTime) < min(endTime, p.second))
-                return false;
-        }
-        books.push_back({startTime, endTime});
+    bool book(int s, int e) {
+        // for (auto p : books) {
+        //     if (max(p.first, startTime) < min(endTime, p.second))
+        //         return false;
+        // }
+        // books.push_back({startTime, endTime});
+        // return true;
+        auto next = mpp.lower_bound(s);
+        if (next != mpp.end() && next->first < e)
+            return false;
+        if (next != mpp.begin() && prev(next)->second > s)
+            return false;
+        mpp[s] = e;
         return true;
     }
 };
