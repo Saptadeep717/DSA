77034d1c32a4d59f04a3cd22484c3abbb0b18ce2
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-05-25 19:24:50 +0530

    Time: 20 ms (27.59%) | Memory: 41.2 MB (63.67%) - LeetSync

---
+++ 852-friends-of-appropriate-ages/friends-of-appropriate-ages.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool request(int a, int b) {
+        return !(b <= 0.5 * a + 7 || b > a || (b > 100 && a < 100));
+    }
+    int numFriendRequests(vector<int>& ages) {
+         sort(ages.begin(), ages.end());
+        int cnt=0;
+        for(auto age : ages){
+            int i=upper_bound(ages.begin(),ages.end(),age)-ages.begin();
+
+             int j=upper_bound(ages.begin(),ages.end(),age*0.5 + 7)-ages.begin();
+
+             cnt+=max(0,i-j-1);
+        }
+
+        return cnt;
+    }
+};
