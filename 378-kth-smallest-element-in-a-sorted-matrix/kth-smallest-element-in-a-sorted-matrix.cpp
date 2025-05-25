460740db1c92bdc0a0baa4c802ff5896a7abaf1e
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-05-26 02:48:52 +0530

    Time: 0 ms (100.00%) | Memory: 17.1 MB (65.74%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
+
+    int possible(vector<vector<int>>& m,int val){
+        int n = m.size();
+        int col = n-1;
+        int c=0;
+        for(int row=0;row<n;row++){
+            while(col>=0 && m[row][col]>val) --col;
+            c+=col+1;
+        }
+        return c;
+    }
     int kthSmallest(vector<vector<int>>& mat, int k) {
         int n = mat.size();
-        vector<int>temp;
+        int start =mat[0][0],end=mat[n-1][n-1];
+        int ans=-1;
+        while(start<=end){
+            int mid = (start+end)/2;
 
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                temp.push_back(mat[i][j]);
+            if(possible(mat,mid) >= k){
+                ans=mid;
+                end=mid-1;
             }
+            else start=mid+1;
+
         }
 
-        sort(temp.begin(),temp.end());
-        //for(auto it:temp) cout<<it<<" ";
-        return temp[k-1];
+        return ans;
 
     }
 };
