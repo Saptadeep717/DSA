1a782b146d6d281f7b67a6e5a166a7a6b02a7f96
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-05-27 17:55:16 +0530

    Time: 5 ms (20.61%) | Memory: 17 MB (5.85%) - LeetSync

---
+++ 1232-sum-of-mutated-array-closest-to-target/sum-of-mutated-array-closest-to-target.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int findBestValue(vector<int>& arr, int target) {
+        int n = arr.size();
+        sort(arr.begin(),arr.end());
+        vector<long long>preSum(n);
+        preSum[0]=arr[0];
+
+        for(int i=1;i<n;i++){
+            preSum[i] = preSum[i-1]+arr[i];
+        }
+
+ int left = 0, right = arr[n - 1];
+    int bestVal = 0;
+    int minDiff = INT_MAX;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        auto it = lower_bound(arr.begin(), arr.end(), mid);
+        int idx = it - arr.begin();
+
+        int currSum = (idx > 0 ? preSum[idx - 1] : 0) + (n - idx) * mid;
+
+        int diff = abs(currSum - target);
+
+        if (diff < minDiff || (diff == minDiff && mid < bestVal)) {
+            bestVal = mid;
+            minDiff = diff;
+        }
+
+        if (currSum < target) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+
+    return bestVal;
+    }
+};
