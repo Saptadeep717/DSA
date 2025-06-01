ff6797167d35641c60b8be819181fed416234964
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-06-01 15:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (60.00%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/reshape-the-matrix.cpp
@@ -2,25 +2,27 @@ class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
         int n = mat.size(), m = mat[0].size();
-        vector<int> v;
+        
         if (n * m != r * c)
             return mat;
 
         vector<vector<int>> ans(r, vector<int>(c));
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                v.push_back(mat[i][j]);
+        
+
+        int row = 0;
+        int col = 0;
+        for(int i=0;i<n; i++){
+            for(int j=0; j<m; j++){
+                ans[row][col] = mat[i][j];
+                col++;
+                if(row<r && col == c){
+                    col =0;
+                    row++;
+                }
             }
         }
-        for (auto it : v)
-            cout << it << " ";
-        int k = 0;
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                ans[i][j] = v[k++];
-            }
-        }
+
         return ans;
     }
 };
