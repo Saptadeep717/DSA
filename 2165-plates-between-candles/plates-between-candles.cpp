12b08dcd0ca0710d7562f89ff1888534f4845a4f
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2025-06-03 04:54:24 +0530

    Time: 19 ms (63.48%) | Memory: 144.9 MB (84.09%) - LeetSync

--- 2165-plates-between-candles/plates-between-candles.cpp
+++ 2165-plates-between-candles/plates-between-candles.cpp
@@ -7,7 +7,8 @@ public:
        }
        for(int q=0;q<queries.size();q++){
         int i = lower_bound(A.begin(), A.end(), queries[q][0]) - A.begin();
-         int j = upper_bound(A.begin(), A.end(), queries[q][1]) - A.begin() ;
+        //  int j = upper_bound(A.begin(), A.end(), queries[q][1]) - A.begin() ;
+          int j = lower_bound(A.begin(), A.end(), queries[q][1]+1) - A.begin() ;
          j--;
         ans.push_back(i < j ? (A[j] - A[i]) - (j - i) : 0);
 
