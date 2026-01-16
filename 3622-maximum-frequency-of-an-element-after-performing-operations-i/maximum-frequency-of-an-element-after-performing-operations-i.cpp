3e3c3356ead202eac3ca2d0f205ccfa5589fdfcd
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-01-16 23:44:47 +0530

    Time: 756 ms (10.15%) | Memory: 205.6 MB (15.22%) - LeetSync

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int numOperations) {
+         unordered_map<int,int>freq;
+        map<int,int>diff;
+        int maxVal = *max_element(begin(nums),end(nums)) + k;
+        for(int i=0;i<nums.size();i++){
+            freq[nums[i]]++;
+
+            int L = max(nums[i]-k , 0);
+            int R = min(nums[i]+k, maxVal);
+            diff[L]+=1;
+            diff[R+1]-=1;
+            diff[nums[i]]+=0;
+        }
+        int cumSum = 0,result=1;
+        for(auto it:diff){
+            int val = it.first;
+            it.second+=cumSum;
+            int freqPresent = freq[val];
+            int neededFreq = it.second - freqPresent;
+            int freqCanBeAcheievd = min(neededFreq, numOperations);
+            result = max(result, freqPresent+freqCanBeAcheievd);
+            cumSum = it.second;
+        }
+
+        return result;
+    }
+};
