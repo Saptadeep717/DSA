bc0237204f4cda38e79f5ba8f00faa51b270ae8d
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-02-01 23:48:02 +0530

    Time: 2 ms (24.78%) | Memory: 12.1 MB (41.81%) - LeetSync

---
+++ 399-evaluate-division/evaluate-division.cpp
@@ -0,0 +1,60 @@
+using P = pair<string, double>;
+class Solution {
+public:
+    double bfs(string& s, string& e, unordered_map<string, vector<P>>& adj) {
+        unordered_set<string> vis;
+        vis.insert(s);
+        double val = 1.0;
+        queue<P> q;
+        q.push({s, 1.0});
+
+        while (!q.empty()) {
+            auto [node, val] = q.front();
+            q.pop();
+            if (node == e)
+                return val;
+            for (auto& it : adj[node]) {
+                if (!vis.count(it.first)) {
+                    vis.insert(it.first);
+                    q.push({it.first, val * it.second});
+                }
+            }
+        }
+        return -1.0;
+    }
+    vector<double> calcEquation(vector<vector<string>>& equations,
+                                vector<double>& values,
+                                vector<vector<string>>& queries) {
+        unordered_map<string, vector<P>> adj;
+        vector<double> ans;
+        for (int i = 0; i < equations.size(); i++) {
+            adj[equations[i][0]].push_back({equations[i][1], values[i]});
+            adj[equations[i][1]].push_back({equations[i][0], 1.00 / values[i]});
+        }
+
+        for (int i = 0; i < queries.size(); i++) {
+            string v1 = queries[i][0], v2 = queries[i][1];
+            if (!adj.count(v1) || !adj.count(v2)) {
+                ans.push_back(-1.0);
+                continue;
+            }
+            if (v1 == v2) {
+                ans.push_back(1.0);
+                continue;
+            }
+
+            double val = bfs(v1, v2, adj);
+            ans.push_back(val);
+        }
+
+        for (auto& it : adj) {
+            cout << it.first << " -> ";
+            for (auto& itt : it.second) {
+                cout << itt.first << " " << itt.second << " ,";
+            }
+            cout << endl;
+        }
+
+        return ans;
+    }
+};
