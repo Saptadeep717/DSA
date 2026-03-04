820f6cb927d4da1b77623a4e4af5bf0d39865421
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-03-04 07:04:07 +0530

    Time: 423 ms (5.10%) | Memory: 91.2 MB (65.10%) - LeetSync

---
+++ 2366-maximum-bags-with-full-capacity-of-rocks/maximum-bags-with-full-capacity-of-rocks.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumBags(vector<int>& capacity, vector<int>& rocks, int additionalRocks) {
+        int n = capacity.size();
+        vector<int>needed(n,0);
+        for(int i=0;i<n;i++){
+            needed[i] = capacity[i]-rocks[i];
+        }
+        long long sum = accumulate(needed.begin(),needed.end(),0LL);
+        if(sum<additionalRocks) return n;
+
+        sort(needed.begin(),needed.end());
+        for(int i=0;i<n;i++){
+            if(needed[i]!=0){
+                if(additionalRocks==0)break;
+                if(additionalRocks>=needed[i]){           
+                    additionalRocks-=needed[i];
+                    needed[i]=0;
+                }
+        
+            }
+        }
+        for(auto it:needed)cout<<it<<" ";
+        int cnt = count(needed.begin(),needed.end(),0);
+        return cnt;
+    }
+};
