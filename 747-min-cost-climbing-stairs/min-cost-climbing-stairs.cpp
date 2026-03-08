a5b7eb72ec0923e1a1143a326ca7eac6328275c3
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-03-08 11:54:05 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (51.47%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
-
-    int solve(int i, vector<int>&v, int n,vector<int>&dp){
-        if(i>=n) return 0;
-        if(dp[i] != -1) return dp[i];
-        int step1 = v[i] + solve(i+1,v,n,dp);
-
-        int step2 = v[i]+solve(i+2,v,n,dp);
-
-        return dp[i]= min(step1,step2);
+    int dp[1001];
+    int solve(int level, vector<int>&cost,int n){
+        //if(level>n) return INT_MAX;
+        if(level==n)return 0;
+        if(dp[level]!=-1) return dp[level];
+        int ans =INT_MAX;
+        for(int step=1;step<=2;step++){
+            if(step+level<=n){
+                ans = min(ans,cost[level]+solve(level+step,cost,n));
+            }
+        }
+        return dp[level] = ans;
     }
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int>dp(n+1,-1);
-
-        int path1 =solve(0,cost,n,dp);
-        int path2 = solve(1,cost,n,dp);
-
-        return min(path1,path2);
+        memset(dp,-1,sizeof(dp));
+        return min(solve(0,cost,n),solve(1,cost,n));
     }
 };
