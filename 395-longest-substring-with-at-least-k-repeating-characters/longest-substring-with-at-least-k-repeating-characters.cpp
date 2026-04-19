ddcff15714ca63dfed92ea79e3602c00661bd816
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-04-20 00:38:27 +0530

    Time: 55 ms (56.92%) | Memory: 11.1 MB (54.17%) - LeetSync

--- 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
+++ 395-longest-substring-with-at-least-k-repeating-characters/longest-substring-with-at-least-k-repeating-characters.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int longestSubstring(string s, int k) {
-       vector<int> freq(26, 0);
-        for (char c : s) freq[c - 'a']++;
-        for (int i = 0; i < s.size(); ++i) {
+    int solve(string &s, int start, int end, int k) {
+        if (end - start + 1 < k) return 0;
+
+        vector<int> freq(26, 0);
+        for (int i = start; i <= end; i++) {
+            freq[s[i] - 'a']++;
+        }
+
+        for (int i = start; i <= end; i++) {
             if (freq[s[i] - 'a'] < k) {
-                int left = longestSubstring(s.substr(0, i), k);
-                int right = longestSubstring(s.substr(i + 1), k);
+                int left = solve(s, start, i - 1, k);
+                int right = solve(s, i + 1, end, k);
                 return max(left, right);
             }
         }
-        return s.size();
+
+        return end - start + 1;
+    }
+
+    int longestSubstring(string s, int k) {
+        return solve(s, 0, s.size() - 1, k);
     }
 };
