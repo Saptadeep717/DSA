c8b50f6b7386cdedda49fd1238ba4235c3104f83
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-03 18:40:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (76.65%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
     string reorganizeString(string s) {
-        vector<int>count(26,0);
         int n = s.size();
-        char maxChar; int maxFreq = 0;
-        for(int i=0;i<n;i++){
-            count[s[i]-'a']++;
-            if(count[s[i]-'a'] > maxFreq){
-                maxFreq = count[s[i]-'a'];
-                maxChar = s[i];
+        vector<int>freq(26,0);
+        for(auto it : s)freq[it-'a']++;
+        int maxCnt =0; char ch;
+        for(int i=0;i<26;i++){
+            if(freq[i] > maxCnt){
+                maxCnt = freq[i];
+                ch = i+'a';
             }
-            
-
-            if(count[s[i]-'a'] > (n+1)/2)return "";
         }
-
-        int i =0;
+        if(maxCnt > (n+1)/2) return "";
         string ans = s;
-        while(count[maxChar-'a'] > 0){
-            ans[i] = maxChar;
+        int i=0;
+        while(freq[ch-'a'] > 0){
+            ans[i] = ch;
+            freq[ch-'a']--;
             i+=2;
-            count[maxChar-'a']--;
         }
-        for(char ch = 'a'; ch<='z';ch++){
-            while(count[ch-'a'] > 0){
-                if(i>=n)i=1;
-                ans[i]=ch;
+
+        for(char c ='a';c<='z';c++){
+            while(freq[c-'a'] > 0){
+                if(i>=n) i=1;
+                ans[i] = c;
+                freq[c-'a']--;
                 i+=2;
-                count[ch-'a']--;
             }
         }
         return ans;
