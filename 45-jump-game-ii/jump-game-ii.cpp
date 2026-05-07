2d92bf242d69db1d459a72f9b9a77e4659071e76
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-07 10:06:27 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (82.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int near=0,far=0, n = nums.size(),jumps=0;
-        while(far<n-1){
-            int farthest = 0;
-            for(int i=near;i<=far;i++){
-                farthest = max(farthest, i+nums[i]);
+        int jumps = 0;
+
+        int maxReachPossible = 0;
+
+        int currentJumpBoundary = 0;
+
+        for(int i = 0; i < nums.size() - 1; i++) {
+
+            maxReachPossible = max(maxReachPossible, i + nums[i]);
+
+            // If current jump can directly reach end
+            if(maxReachPossible >= nums.size() - 1) {
+                return jumps + 1;
             }
-            near = far+1;
-            far=farthest;
-            jumps+=1;
+
+            // current range exhausted
+            if(i == currentJumpBoundary) {
+
+                jumps++;
+
+                currentJumpBoundary = maxReachPossible;
+            }
         }
+
         return jumps;
-        
     }
 };
