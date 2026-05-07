e78d7626789388bcfb11f4a4b073a130a60eea0a
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-07 23:39:11 +0530

    Time: 3 ms (28.62%) | Memory: 21.7 MB (77.12%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.cpp
+++ 735-asteroid-collision/asteroid-collision.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    vector<int> asteroidCollision(vector<int>& ast) {
-        int n = ast.size();
-        vector<int> temp;
-        for (int i = 0; i < n; i++) {
-            if (ast[i] > 0) {
-                temp.push_back(ast[i]);
-            } else {
-                while (!temp.empty() && temp.back() > 0 &&
-                       temp.back() < abs(ast[i])) {
-                    temp.pop_back();
-                }
-                if (!temp.empty() && temp.back() == abs(ast[i]))
-                    temp.pop_back();
-                else if(temp.empty() || temp.back() < 0) temp.push_back(ast[i]);    
+    vector<int> asteroidCollision(vector<int>& a) {
+        int n = a.size();
+        vector<int>temp;
+        for(int i=0;i<n;i++){
+            if(a[i] > 0) temp.push_back(a[i]);
+            else{
+                while(!temp.empty() && temp.back() > 0 && temp.back() < abs(a[i])) temp.pop_back();
+                if(!temp.empty() && temp.back() == abs(a[i])) temp.pop_back();
+                else if(temp.empty() || temp.back() < 0) temp.push_back(a[i]);
             }
         }
         return temp;
