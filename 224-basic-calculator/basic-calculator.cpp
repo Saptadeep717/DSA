bcedc173fe2fed77764265f742cb7b4847a8c461
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-10 02:04:27 +0530

    Time: 7 ms (25.28%) | Memory: 13.7 MB (44.21%) - LeetSync

--- 224-basic-calculator/basic-calculator.cpp
+++ 224-basic-calculator/basic-calculator.cpp
@@ -1,77 +1,47 @@
 class Solution {
 public:
     int calculate(string s) {
-        long long res = 0;   // current result
-        long long num = 0;   // current number being built
-        int sign = 1;        // +1 or -1
-
-        stack<long long> st; // stores previous state
-
-        for (auto c : s) {
-
-            // ------------------------
-            // CASE 1: digit → build number
-            // ------------------------
-            if (isdigit(c)) {
-                num = num * 10 + (c - '0');
+        long long currentFormedNum=0, currentFormedResult=0;
+        int sign = 1;
+        stack<int>st;
+        for(char c : s){
+            if(c ==' ') continue;
+            if(isdigit(c)){
+                currentFormedNum = currentFormedNum*10 + (c-'0');
+                continue;
             }
-
-            // ------------------------
-            // CASE 2: '+'
-            // finalize previous number
-            // ------------------------
-            else if (c == '+') {
-                res += num * sign;
-                num = 0;
+            if(c == '+'){
+                currentFormedResult += currentFormedNum*sign;
                 sign = 1;
+                currentFormedNum = 0;
             }
 
-            // ------------------------
-            // CASE 3: '-'
-            // ------------------------
-            else if (c == '-') {
-                res += num * sign;
-                num = 0;
+            if(c == '-'){
+                currentFormedResult += currentFormedNum*sign;
                 sign = -1;
+                currentFormedNum = 0;
             }
 
-            // ------------------------
-            // CASE 4: '('
-            // Save current state
-            // ------------------------
-            else if (c == '(') {
-                st.push(res);   // save result so far
-                st.push(sign);  // save sign before '('
-
-                // reset for new expression inside ()
-                res = 0;
+            if(c == '('){
+                st.push(currentFormedResult);
+                st.push(sign);
+                currentFormedResult=0;
+                currentFormedNum = 0;
                 sign = 1;
-                num = 0;
             }
 
-            // ------------------------
-            // CASE 5: ')'
-            // Resolve inner expression
-            // ------------------------
-            else if (c == ')') {
-                res += num * sign; // finalize inside
-
-                num = 0;
-
-                int prevSign = st.top(); st.pop();
-                int prevRes  = st.top(); st.pop();
-
-                // apply sign before '('
-                res = res * prevSign;
-
-                // add to previous result
-                res = res + prevRes;
+            if(c == ')'){
+                currentFormedResult += currentFormedNum*sign;
+                currentFormedNum = 0;
+                int prevSign = st.top();
+                st.pop();
+                int prevVal = st.top();
+                st.pop();
+                currentFormedResult = prevVal + (currentFormedResult * prevSign);
             }
+            
         }
-
-        // last number (important)
-        res += num * sign;
-
-        return res;
+        currentFormedResult += currentFormedNum*sign;
+        return (int)currentFormedResult;
     }
 };
