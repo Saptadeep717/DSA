00a07d72d1fd0126ca473ebceb745efe962768b5
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-23 12:51:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (85.77%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,27 +1,12 @@
 class Solution {
 public:
     int dp[46];
-    int solve(int level, int n){
-        //pruning
-
-        //base case
-        if(level==n) return 1;
-
-        //caching
-        if(dp[level]!= -1) return dp[level];
-        int ans =0;
-        //choices 
-        for(int step=1;step<=2;step++){
-            //check
-            if(level+step <= n){
-                // move
-                ans+=solve(level+step,n);
-            }
-        }
-        return dp[level] = ans;
-    }
+    
     int climbStairs(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(0,n);
+        dp[0]=1,dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
