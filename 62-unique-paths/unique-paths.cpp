1a699e4866a255dbc616e2d013cac10e21004ffe
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-23 14:12:54 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (28.16%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -9,15 +9,19 @@ public:
         // There is exactly 1 way to stand at starting cell
         dp[0][0] = 1;
 
+        vector<int>prev(n,0);
         // Traverse the grid row by row
         for(int r = 0; r < m; r++) {
-
+            vector<int>curr(n,0);
             for(int c = 0; c < n; c++) {
 
                 // Skip the starting cell
                 // because we already filled it
-                if(r == 0 && c == 0)
-                    continue;
+                if(r == 0 && c == 0){
+                    curr[c] = 1;
+                     continue;
+                }
+                   
 
                 int up = 0;
                 int left = 0;
@@ -25,19 +29,24 @@ public:
                 // If upper cell exists,
                 // take paths coming from top
                 if(r > 0)
-                    up = dp[r - 1][c];
+                    //up = dp[r - 1][c];
+                    up = prev[c];
 
                 // If left cell exists,
                 // take paths coming from left
                 if(c > 0)
-                    left = dp[r][c - 1];
+                    //left = dp[r][c - 1];
+                    left = curr[c - 1];
 
                 // Total paths to current cell
-                dp[r][c] = up + left;
+                //dp[r][c] = up + left;
+                curr[c] = up + left;
             }
+            prev = curr;
         }
 
         // Bottom-right cell contains final answer
-        return dp[m - 1][n - 1];
+        //return dp[m - 1][n - 1];
+        return prev[n-1];
     }
 };
