033cd76d03ee303ae402811de00073135cdf4aa4
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-24 02:49:52 +0530

    Time: 90 ms (37.35%) | Memory: 14.5 MB (42.53%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,34 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> temp;
-        temp.push_back(nums[0]);
-        int len = 1;
-        for(int i =1; i<nums.size(); i++){
-            if(nums[i]> temp.back()){
-                temp.push_back(nums[i]);
-                len++;
-                continue;
-                    
+        int n = nums.size();
+        vector<int>dp(n,1);
+        int maxi = 1;
+        vector<int>parent(n);
+        int lastIndex=0;
+        for(int i=0;i<n;i++)parent[i]=i;
+        for(int i = 0; i<n; i++ ){
+            for(int prev = 0; prev<i; prev++){
+                if(nums[prev]< nums[i] && dp[i] < 1+dp[prev]){
+                    dp[i] =  1+dp[prev];
+                    parent[i] = prev;
+                }
             }
-        
-        int ind = lower_bound(temp.begin(), temp.end(), nums[i]) - temp.begin();
-        temp[ind] = nums[i];
-    }
-    return len;
+            if(dp[i] > maxi){
+                maxi = dp[i];
+                lastIndex=i;
+            }
+            
+        }
+        vector<int>lis;
+        lis.push_back(nums[lastIndex]);
+        while(parent[lastIndex] != lastIndex){  
+            lastIndex = parent[lastIndex];
+            lis.push_back(nums[lastIndex]);     
+        }
+        for(auto it: lis)cout<<it<<" ";
+
+        return maxi;
+    
     }
 };
