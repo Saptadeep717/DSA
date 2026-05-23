45e1d9c8817fbc32252b83b241615b13e86e0c2a
Saptadeep717 <76860444+Saptadeep717@users.noreply.github.com>
2026-05-23 18:50:36 +0530

    Time: 38 ms (35.32%) | Memory: 36.7 MB (38.89%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+
+        vector<vector<unsigned int>> dp(
+            n + 1,
+            vector<unsigned int>(amount + 1, 0)
+        );
+
+        // One way to form amount 0:
+        // choose nothing
+        dp[n][0] = 1;
+
+        for(int ind = n - 1; ind >= 0; ind--) {
+
+            for(int a = 0; a <= amount; a++) {
+
+                unsigned int notTake =
+                    dp[ind + 1][a];
+
+                unsigned int take = 0;
+
+                if(coins[ind] <= a) {
+
+                    take =
+                        dp[ind][a - coins[ind]];
+                }
+
+                dp[ind][a] = take + notTake;
+            }
+        }
+
+        return (int)dp[0][amount];
+    }
+};
